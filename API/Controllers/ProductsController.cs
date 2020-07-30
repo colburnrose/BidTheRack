35fcc108d7cccd6de1c719140a8f8b8ba2dec58a
colburnrose <colburnsanders@gmail.com>
2020-07-30 16:43:50 -0500

    repo pattern and migrations

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -13,23 +10,23 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _ctx;
-        public ProductsController(StoreContext ctx)
+        private readonly IProductRepository _productRepository;
+        public ProductsController(IProductRepository productRepository)
         {
-            _ctx = ctx;
+            _productRepository = productRepository;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _ctx.Products.ToListAsync();
+            var products = await _productRepository.GetProductsAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _ctx.Products.FindAsync(id);
+            return await _productRepository.GetProductByIdAsync(id);
         }
     }
 }
