53fe8c25f6a0fa8d673e42252e39f622d37e24eb
colburnrose <colburnsanders@gmail.com>
2020-07-30 16:57:40 -0500

    repo pattern and migrations

---
+++ Infrastructure/Data/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _ctx;
+        public ProductRepository(StoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            return await _ctx.Products.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync() => await _ctx.Products.ToListAsync();
+    }
+}
