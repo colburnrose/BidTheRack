8be1c26ba12df42945494839ab2e5f681b688cda
colburnrose <colburnsanders@gmail.com>
2020-07-31 13:04:46 -0500

    data seed and eager loading

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -23,6 +23,20 @@ namespace API.Controllers
             return Ok(products);
         }
 
+        [HttpGet("brands")]
+        public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+        {
+            var brands = await _productRepository.GetProductBrandsAsync();
+            return Ok(brands);
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProducTypes()
+        {
+            var types = await _productRepository.GetProductTypesAsync();
+            return Ok(types);
+        }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
