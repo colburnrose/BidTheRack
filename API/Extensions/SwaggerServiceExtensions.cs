56ae4c15fb054161e4671315ad64111d90609b88
colburnrose <colburnsanders@gmail.com>
2020-08-05 20:38:02 -0500

    cleaned up startup class

---
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(v =>
+            {
+                v.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "BidTheRack",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Colburn Sanders",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/colburnomareo")
+                    }
+                });
+            });
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "BidTheRack API v1"); });
+            return app;
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,19 +1,13 @@
-using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
-using Infrastructure.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Linq;
 
 namespace API
 {
@@ -28,44 +22,13 @@ namespace API
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+        {        
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.InvalidModelStateResponseFactory = response =>
-                {
-                    var errors = response.ModelState
-                    .Where(e => e.Value.Errors.Count > 0)
-                    .SelectMany(x => x.Value.Errors)
-                    .Select(x => x.ErrorMessage).ToArray();
-
-                    var errorResponse = new ApiValidationResponse
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
-            services.AddSwaggerGen(v =>
-            {
-                v.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "BidTheRack",
-                    Version = "v1",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Colburn Sanders",
-                        Email = string.Empty,
-                        Url = new Uri("https://twitter.com/colburnomareo")
-                    }
-                });
-            });
+            services.AddApplicationServices();
+            services.AddSwaggerServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -79,9 +42,7 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "BidTheRack API v1"); });
-
+            app.UseSwaggerDocumentation();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
