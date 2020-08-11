baf1ee74833ed05017daa8f12af7fcc664f5fc38
colburnrose <colburnsanders@gmail.com>
2020-08-11 18:12:45 -0500

    routing and lazy loading

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
+    {path: '**', redirectTo: '', pathMatch: 'full'}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
