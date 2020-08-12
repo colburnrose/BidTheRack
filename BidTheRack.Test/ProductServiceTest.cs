ee41b8628f9573abb4d8c6b2e0c045d1c978d68f
colburnrose <colburnsanders@gmail.com>
2020-08-12 18:59:45 -0500

    breadcrumb service and updated theme

---
+++ BidTheRack.Test/ProductServiceTest.cs
@@ -0,0 +1,31 @@
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BidTheRack.Test
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        public ProductServiceTest()
+        {
+        }
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+
+        }
+
+
+    }
+}
