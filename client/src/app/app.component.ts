e74133695d2295f4d4f130d69def49176d99d7f9
colburnrose <colburnsanders@gmail.com>
2020-08-28 16:02:39 -0500

    added basketservice, and basket service methods

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 
 @Component({
@@ -10,7 +11,21 @@ export class AppComponent implements OnInit {
 title = 'BidTheRack';
 
 
-constructor() {}
+constructor(private basketService: BasketService) {}
 
-  ngOnInit(): void{}
+  ngOnInit(): void{
+    this.loadBasket();
+  }
+
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId){
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+  
 }
---
+++ client/src/app/basket/basket.service.ts
@@ -0,0 +1,80 @@
+
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { IBasket, IBasketItem, Basket } from '../shared/models/basket';
+import { IProduct } from '../shared/models/product';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasketService {
+  baseUrl = environment.apiUrl;
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  basket$ = this.basketSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  getBasket(id: string){
+    return this.http.get(this.baseUrl + 'basket?id=' + id)
+    .pipe(
+      map((basket: IBasket) => {
+        this.basketSource.next(basket);
+        console.log(this.getCurrentBasket());
+      })
+    );
+  }
+
+  setBasket(basket: IBasket){
+    return this.http.post(this.baseUrl + 'basket', basket).subscribe((response: IBasket) => {
+      this.basketSource.next(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getCurrentBasket() {
+    return this.basketSource.value;
+  }
+
+  addItemToBasket(item: IProduct, quantity = 1) {
+    const itemToAdd: IBasketItem = this.mapProductItemToBasket(item, quantity);
+    const basket = this.getCurrentBasket() ?? this.createBasket();
+    basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity)
+    this.setBasket(basket);
+  }
+
+  private addOrUpdateItem(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number): IBasketItem[] {
+    console.log(items);
+    const index = items.findIndex(i => i.id === itemToAdd.id);
+    if(index === -1){
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    } else {
+      items[index].quantity += quantity;
+    }
+    return items;
+  }
+
+  private createBasket(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  private mapProductItemToBasket(item: IProduct, quantity: number): IBasketItem {
+    var product = {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      quantity,
+      imageUrl: item.imageUrl,
+      brand: item.productBrand,
+      type: item.productType
+    };
+    return product;
+  }
+}
